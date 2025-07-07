63ce4267fd4b37185e3a5104825b99931e5684af
BearAzul <adyaabduazizulhakim@gmail.com>
2025-07-07 12:59:56 +0700

    mobile offline

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Chat Me App",
+        short_name: "Chat Me App",
+        description: "A Simple Chat Application using MERN stack",
+        theme_color: "#0f172a",
+        background_color: "#0f172a",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "192_chat_logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512_chat_logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
